75bb2a54e734ba0b339fbfdb6138ff0c51652085
Anand Graves <anand@voorhoede.nl>
2019-07-16 12:04:53 +0200

    feat: read twitter card markup

---
+++ src/content.js
@@ -0,0 +1,14 @@
+const url = window.location.href
+const twitterCard = document.querySelector('meta[name="twitter:card"]').getAttribute('content')
+const twitterTitle = document.querySelector('meta[name="twitter:title"]').getAttribute('content')
+const twitterDescription = document.querySelector('meta[name="twitter:description"]').getAttribute('content')
+const twitterImage = document.querySelector('meta[name="twitter:image"]').getAttribute('content')
+
+const twitter = {
+  twitterCard,
+  twitterTitle,
+  twitterDescription,
+  twitterImage
+}
+
+console.log(twitter)
