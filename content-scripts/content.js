a45a86983695b26aaf7f06fb3be6b7471bb08cf7
Anand Graves <anand@voorhoede.nl>
2019-07-22 16:34:50 +0200

    feat: update panel on <meta> mutations

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,67 +1,91 @@
 "use strict";
 
-const url = window.location.href
-const twitterCard = document.querySelector('meta[name="twitter:card"]')
-const twitterTitle = document.querySelector('meta[name="twitter:title"]')
-const twitterDescription = document.querySelector('meta[name="twitter:description"]')
-const twitterImage = document.querySelector('meta[name="twitter:image"]')
-const openGraphType = document.querySelector('meta[property="og:type"]')
-const openGraphTitle = document.querySelector('meta[property="og:title"]')
-const openGraphImage = document.querySelector('meta[property="og:image"]')
-const openGraphUrl = document.querySelector('meta[property="og:url"]')
-const openGraphDescription = document.querySelector('meta[property="og:description "]')
-const openGraphAudio = document.querySelector('meta[property="og:audio "]')
-const openGraphVideo = document.querySelector('meta[property="og:video "]')
-const metaTitle = document.getElementsByTagName("title")[0].innerHTML
-const metaViewport = document.querySelector('[name="viewport"]')
+function getHeadData() {
+  const url = window.location.href
+  const twitterCard = document.querySelector('meta[name="twitter:card"]')
+  const twitterTitle = document.querySelector('meta[name="twitter:title"]')
+  const twitterDescription = document.querySelector('meta[name="twitter:description"]')
+  const twitterImage = document.querySelector('meta[name="twitter:image"]')
+  const openGraphType = document.querySelector('meta[property="og:type"]')
+  const openGraphTitle = document.querySelector('meta[property="og:title"]')
+  const openGraphImage = document.querySelector('meta[property="og:image"]')
+  const openGraphUrl = document.querySelector('meta[property="og:url"]')
+  const openGraphDescription = document.querySelector('meta[property="og:description "]')
+  const openGraphAudio = document.querySelector('meta[property="og:audio "]')
+  const openGraphVideo = document.querySelector('meta[property="og:video "]')
+  const metaTitle = document.getElementsByTagName("title")[0].innerHTML
+  const metaViewport = document.querySelector('[name="viewport"]')
 
-const pageMeta = {
-  title: metaTitle,
-  items: showItems([
-    { title: getMetaName(metaViewport), value: getMetaContent(metaViewport) }
-  ])
-}
+  const pageMeta = {
+    title: metaTitle,
+    items: showItems([
+      { title: getMetaName(metaViewport), value: getMetaContent(metaViewport) }
+    ])
+  }
 
-const twitter = {
-  title: 'Twitter',
-  items: showItems([
-    { title: getMetaName(twitterCard), value: getMetaContent(twitterCard) },
-    { title: getMetaName(twitterTitle), value: getMetaContent(twitterTitle) },
-    { title: getMetaName(twitterDescription), value: getMetaContent(twitterDescription) },
-    { title: getMetaName(twitterImage), value: getMetaContent(twitterImage) },
-  ])
-}
+  const twitter = {
+    title: 'Twitter',
+    items: showItems([
+      { title: getMetaName(twitterCard), value: getMetaContent(twitterCard) },
+      { title: getMetaName(twitterTitle), value: getMetaContent(twitterTitle) },
+      { title: getMetaName(twitterDescription), value: getMetaContent(twitterDescription) },
+      { title: getMetaName(twitterImage), value: getMetaContent(twitterImage) },
+    ])
+  }
 
-const openGraph = {
-  title: 'Twitter',
-  items: showItems([
-    { title: getMetaProperty(openGraphType), value: getMetaContent(openGraphType) },
-    { title: getMetaProperty(openGraphTitle), value: getMetaContent(openGraphTitle) },
-    { title: getMetaProperty(openGraphImage), value: getMetaContent(openGraphImage) },
-    { title: getMetaProperty(openGraphUrl), value: getMetaContent(openGraphUrl) },
-    { title: getMetaProperty(openGraphDescription), value: getMetaContent(openGraphDescription) },
-    { title: getMetaProperty(openGraphAudio), value: getMetaContent(openGraphAudio) },
-    { title: getMetaProperty(openGraphVideo), value: getMetaContent(openGraphVideo) },
-  ])
-}
+  const openGraph = {
+    title: 'Twitter',
+    items: showItems([
+      { title: getMetaProperty(openGraphType), value: getMetaContent(openGraphType) },
+      { title: getMetaProperty(openGraphTitle), value: getMetaContent(openGraphTitle) },
+      { title: getMetaProperty(openGraphImage), value: getMetaContent(openGraphImage) },
+      { title: getMetaProperty(openGraphUrl), value: getMetaContent(openGraphUrl) },
+      { title: getMetaProperty(openGraphDescription), value: getMetaContent(openGraphDescription) },
+      { title: getMetaProperty(openGraphAudio), value: getMetaContent(openGraphAudio) },
+      { title: getMetaProperty(openGraphVideo), value: getMetaContent(openGraphVideo) },
+    ])
+  }
 
-const headData = {
-  url,
-  pageMeta,
-  twitter,
-  openGraph
+  const headData = {
+    url,
+    pageMeta,
+    twitter,
+    openGraph
+  }
+
+  return headData
 }
 
 // Send data to the background script.
-chrome.runtime.sendMessage(headData)
-
+chrome.runtime.sendMessage(getHeadData())
 
 // Listen to messages from the background script.
-chrome.runtime.onMessage.addListener(function(message) {
+chrome.runtime.onMessage.addListener(function(message, id) {
+  console.log('message', message, 'id', id)
   if (message && message.action === 'reload-panel') {
-    chrome.runtime.sendMessage(headData)
+    chrome.runtime.sendMessage(getHeadData())
   }
 })
+
+const headElement = document.querySelector('head')
+const observerOptions = {
+  childList: true,
+  attributes: true,
+  subtree: true,
+  characterData: true
+}
+const observer = new MutationObserver(function (mutationList) {
+  mutationList.forEach((mutation) => {
+    if (mutation.type === 'attributes' && mutation.target.nodeName === 'META') {
+      /* An attribute value changed on the element in
+           mutation.target; the attribute name is in
+           mutation.attributeName and its previous value is in
+           mutation.oldValue */
+      chrome.runtime.sendMessage(getHeadData())
+    }
+  })
+})
+observer.observe(headElement, observerOptions)  
 
 function showItems(items) {
   return items.filter(item => item.title && item.value)
