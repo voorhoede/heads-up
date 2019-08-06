b4104f2e0357b67e58fa210eda48b862cc93d08d
Anand Graves <anand@voorhoede.nl>
2019-08-06 15:54:17 +0200

    refactor: copied markup/styles/scripting from twitter preview small

--- src/components/twitter-preview/twitter-large.js
+++ src/components/twitter-preview/twitter-large.js
@@ -1,53 +1,55 @@
-init()
-
-function init() {
+(function(window) {
   const params = (new URL(window.location.href)).searchParams
   const title = params.get('title')
   const description = params.get('description')
   const image = params.get('image')
-  const hostname = params.get('hostname')
-  const twitterData = {
+  const url = params.get('url')
+
+  const twitterElement = document.querySelector('[data-twitter-preview-card]')
+  twitterElement.innerHTML = getTwitterMarkup({
     title,
     description,
     image,
-    hostname
-  }
+    url
+  })
 
-  renderTwitterCard(twitterData)
-}
+  function getTwitterMarkup({ title, description, image, url }) {
+      const twitterLink = url ? `href="${ url }"` : ''
+      const imageMarkup = `<img src="${ image }" class="twitter-preview-large__image" />`
 
-function renderTwitterCard(data) {
-  const twitterElement = document.querySelector('[data-twitter-preview-card]')
-  twitterElement.innerHTML = getTwitterMarkup(data)
-}
+      function getHostName(url) {
+        if (!url) {
+          return ''
+        }
 
-function getTwitterMarkup(data) {
-  const { title, image, description, hostname } = data
-  return `
-  <div class="TwitterCardsGrid TwitterCard TwitterCard--animation">
-    <div class="TwitterCardsGrid-col--12 TwitterCardsGrid-col--spacerBottom CardContent">
-      <a class="js-openLink u-block TwitterCardsGrid-col--12 TwitterCard-container TwitterCard-container--clickable SummaryCard--large"
-        href="" rel="noopener" data-card-breakpoints="w400 w350 w300 w250 w200 w150 w100 w50 ">
-        <div class="SummaryCard-image TwitterCardsGrid-col--12">
-          <div class="tcu-imageContainer tcu-imageAspect--2to1" style="max-height: 200px">
-            <div class="tcu-imageWrapper"
-              style="opacity: 1; background-image: url(&quot;${ image}&quot;); background-size: cover;"
-              data-style="background-image: url(${ image}); background-size: cover;">
-              <img class="u-block"
-                alt=""
-                src="${ image}">
+        const hostname = (new URL(url).hostname)
+        if (hostname.startsWith('www.')) {
+          return hostname
+          .split('.')
+          .slice(1)
+          .join('.')
+        }
+        return hostname
+      }
+
+      return `
+        <div class="twitter-preview-large">
+          <a ${ twitterLink } class="twitter-preview-large__link-container">
+            <div class="${ image
+              ? `twitter-preview-large__media`
+              : `twitter-preview-large__media twitter-preview-large__media--image-fallback` }">
+              ${ image ? `${ imageMarkup }` : `<div class="twitter-preview-large__image-fallback"></div>` }
             </div>
-          </div>
+
+            <div class="twitter-preview-large__content">
+              <div class="twitter-preview-large__title">${ title }</div>
+              <div class="twitter-preview-large__description">${ description }</div>
+              <div class="twitter-preview-large__domain">
+                <svg viewBox="0 0 24 24" class="twitter-preview-large__icon"><g><path d="M11.96 14.945c-.067 0-.136-.01-.203-.027-1.13-.318-2.097-.986-2.795-1.932-.832-1.125-1.176-2.508-.968-3.893s.942-2.605 2.068-3.438l3.53-2.608c2.322-1.716 5.61-1.224 7.33 1.1.83 1.127 1.175 2.51.967 3.895s-.943 2.605-2.07 3.438l-1.48 1.094c-.333.246-.804.175-1.05-.158-.246-.334-.176-.804.158-1.05l1.48-1.095c.803-.592 1.327-1.463 1.476-2.45.148-.988-.098-1.975-.69-2.778-1.225-1.656-3.572-2.01-5.23-.784l-3.53 2.608c-.802.593-1.326 1.464-1.475 2.45-.15.99.097 1.975.69 2.778.498.675 1.187 1.15 1.992 1.377.4.114.633.528.52.928-.092.33-.394.547-.722.547z"></path><path d="M7.27 22.054c-1.61 0-3.197-.735-4.225-2.125-.832-1.127-1.176-2.51-.968-3.894s.943-2.605 2.07-3.438l1.478-1.094c.334-.245.805-.175 1.05.158s.177.804-.157 1.05l-1.48 1.095c-.803.593-1.326 1.464-1.475 2.45-.148.99.097 1.975.69 2.778 1.225 1.657 3.57 2.01 5.23.785l3.528-2.608c1.658-1.225 2.01-3.57.785-5.23-.498-.674-1.187-1.15-1.992-1.376-.4-.113-.633-.527-.52-.927.112-.4.528-.63.926-.522 1.13.318 2.096.986 2.794 1.932 1.717 2.324 1.224 5.612-1.1 7.33l-3.53 2.608c-.933.693-2.023 1.026-3.105 1.026z"></path></g></svg>
+                <div class="twitter-preview-large__hostname">${ getHostName(url) }</div></div>
+            </div>
+          </a>
         </div>
-        <div class="SummaryCard-contentContainer TwitterCardsGrid-col--12">
-          <div class="SummaryCard-content TwitterCardsGrid-ltr">
-            <h2 class="TwitterCard-title js-cardClick tcu-textEllipse--multiline" dir="ltr">${ title}</h2>
-            <p class="tcu-resetMargin u-block TwitterCardsGrid-col--spacerTop tcu-textEllipse--multiline" dir="ltr">${ description}</p>
-            <span class="u-block TwitterCardsGrid-col--spacerTop SummaryCard-destination" dir="ltr">${ hostname}</span>
-          </div>
-        </div>
-      </a>
-    </div>
-  </div>
-  `
-}
+      `
+  }
+})(window)
