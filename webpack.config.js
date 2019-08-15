6a34840df3199563817544aa5a0fbe7a8d4d6f98
Declan Rek <declanrek@gmail.com>
2019-08-15 17:45:28 +0200

    add webpack for building and development

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const createConfig = require('./createConfig')
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = createConfig({
+  entry: {
+    devtools: './src/devtools.js',
+    background: './src/background.js',
+    content: './src/content.js',
+    'heads-up': './src/heads-up/index.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'heads-up.html',
+      template: 'src/heads-up/index.html',
+      chunks: ['heads-up'],
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'devtools.html',
+      template: 'src/devtools.html',
+      chunks: []
+    }),
+    new CopyPlugin([
+      { from: 'src/assets/', to: 'assets' },
+      { from: 'src/manifest.json', to: 'manifest.json' },
+    ]),
+  ],
+  devtool: process.env.NODE_ENV !== 'production'
+    ? '#inline-source-map'
+    : false
+})
