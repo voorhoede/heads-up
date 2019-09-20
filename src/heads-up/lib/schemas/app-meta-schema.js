0ada75e71f4647663d131313c8578ed733573a41
Anand Graves <anand@voorhoede.nl>
2019-09-20 13:31:16 +0200

    refactor: removed test data

--- src/heads-up/lib/schemas/app-meta-schema.js
+++ src/heads-up/lib/schemas/app-meta-schema.js
@@ -1,13 +1,8 @@
-  const appMetaSchema = {
+const appMetaSchema = {
   'title': {
     required: true,
-    use: ['hasLetterA', 'hasLength5'],
     message: {
-      required: `The title element is required.`,
-      use: [
-        `Title has no letter A.`,
-        `The Title Length is not exactly 5 characters long.`
-      ]
+      required: `The title element is required.`
     },
     meta: {
       info: `The HTML <code>title</code> element defines the document's title that is shown in a browser's title bar or a page's tab.`,
@@ -17,7 +12,6 @@
 
   'lang': {
     required: true,
-    enum: ['de', 'fr'],
     message: {
       required: `The lang attribute is required.`,
       enum: `Lang attribute does not contain a correct value.`
@@ -30,14 +24,8 @@
 
   'charset': {
     required: true,
-    length: { min: 5, max: 32 },
-    match: /^utf-8$/,
-    use: ['hasLetterA'],
     message: {
       required: `The charset attribute is required.`,
-      length: `The charset attribute does not have the correct length.`,
-      match: `The charset attribute does not have the correct value.`,
-      use: [`Charset has no letter A.`],
     },
     meta: {
       info: `The meta attribute <code>charset</code> specifies which character set a web page is written with.`,
@@ -46,12 +34,6 @@
   },
 
   'viewport': {
-    use: ['hasLetterA'],
-    message: {
-      use: [
-        'Viewport has no letter A.'
-      ]
-    },
     meta: {
       info: `The <code>viewport</code> is the part of the web page that the user can currently see.`,
       link: 'https://webhint.io/docs/user-guide/hints/hint-meta-viewport/'
@@ -59,13 +41,6 @@
   },
 
   'theme-color': {
-    use: ['hasLetterA', 'hasLength5'],
-    message: {
-      use: [
-        `Theme color has no letter A.`,
-        `The theme color length is not exactly 5 characters long.`
-      ]
-    },
     meta: {
       info: `Set the toolbar color of the Chrome browser.`,
       link: 'https://developers.google.com/web/updates/2014/11/Support-for-theme-color-in-Chrome-39-for-Android'
