165e724ca2af0c0b985249b0bfe38904e95a0a41
Anand Graves <anand@voorhoede.nl>
2019-09-23 10:18:44 +0200

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
-    'vue/no-v-html': 0
+    'vue/no-v-html': 0,
+    'vue/singleline-html-element-content-newline': 0
   }
 }
