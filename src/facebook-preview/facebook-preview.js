4d55d590c63bf01e9863eb481c32081bb25c11a9
Robin Stut <robin@voorhoede.nl>
2019-11-13 10:33:05 +0100

    fix: writing facebook to promise

--- src/facebook-preview/facebook-preview.js
+++ src/facebook-preview/facebook-preview.js
@@ -2,65 +2,64 @@
 createPreview()
 
 function createPreview() {
-    const params = (new URL(window.location.href)).searchParams
-    const title = params.get('title')
-    const description = params.get('description')
-    const image = params.get('image')
-    const url = params.get('url')
-    const type = params.get('card')
-    const theme = params.get('theme')
+  const params = (new URL(window.location.href)).searchParams
+  const title = params.get('title')
+  const description = params.get('description')
+  const imgString = params.get('image')
+  const url = params.get('url')
+  const type = params.get('card')
+  const theme = params.get('theme')
 
-    const facebookElement = document.querySelector('[data-facebook-preview-card]')
-    facebookElement.innerHTML = getfacebookMarkup({
-        title,
-        description,
-        image,
-        url,
-        type
-    })
+  const facebookElement = document.querySelector('[data-facebook-preview-card]')
+  getfacebookMarkup({
+    title,
+    description,
+    imgString,
+    url,
+    type
+  }).then(html => facebookElement.innerHTML = html)
 
-    /**
-     * setting 'theme_dark' class if parent window is in dark mode
-     * class '-theme-with-dark-background' is taken from dev tools env
-     * src: https://github.com/ChromeDevTools/devtools-frontend/blob/02a851d01de158d8c0a8fd1d3af06649b5379bd6/front_end/ui/inspectorStyle.css
-     */
-    if (theme === 'dark') document.body.classList.add('-theme-with-dark-background')
+  /**
+   * setting 'theme_dark' class if parent window is in dark mode
+   * class '-theme-with-dark-background' is taken from dev tools env
+   * src: https://github.com/ChromeDevTools/devtools-frontend/blob/02a851d01de158d8c0a8fd1d3af06649b5379bd6/front_end/ui/inspectorStyle.css
+   */
+  if (theme === 'dark') document.body.classList.add('-theme-with-dark-background')
 }
 
 
-function getfacebookMarkup({ title, description, image, url, type }) {
-    const facebookLink = url ? `href="${url}"` : ''
+function getfacebookMarkup({ title, description, imgString, url, type }) {
+  const facebookLink = url ? `href="${url}"` : ''
 
-    function getHostName(url) {
-        if (!url) {
-            return ''
-        }
+  function getMeta(url) {
+    return new Promise((resolve, reject) => {
+      var img = new Image();
+      img.src = url;
+      img.onload = () => resolve(img)
+    })
+  }
 
-        const hostname = (new URL(url).hostname)
-        const wwwPrefix = 'www.'
-        return hostname.startsWith(wwwPrefix)
-            ? hostname.slice(wwwPrefix.length)
-            : hostname
+  return getMeta(imgString).then(img => generateHtml({ img, title, description, imgString, url, type }))
+}
 
-    }
-
-    return `
+function generateHtml({ img, title, description, imgString, url, type }) {
+  console.log(img.width === img.height);
+  return `
       <div class="${ type === 'summary' ? `facebook-preview is-small` : `facebook-preview`}">
         <a rel="noopener" target="_blank" class="${ type === 'summary' ? `facebook-preview__link-container` : `facebook-preview__link-container facebook-preview__link-container--vertical`} ">
-          <div class="${ image
-            ? `${type === 'summary'
-                ? `facebook-preview__media`
-                : `facebook-preview__fixed-ratio facebook-preview__ratio`}`
-            : `facebook-preview__media facebook-preview__media--image-fallback`}">
-            ${ image
-            ? `<img src="${image}" class="${type === 'summary'
-                ? `facebook-preview__image`
-                : `facebook-preview__fixed-ratio-content`}" />`
-            : `<div class="facebook-preview__image-fallback"></div>`}
+          <div class="${ imgString
+      ? `${type === 'summary'
+        ? `facebook-preview__media`
+        : `facebook-preview__fixed-ratio facebook-preview__ratio`}`
+      : `facebook-preview__media facebook-preview__media--image-fallback`}">
+            ${ imgString
+      ? `<img src="${imgString}" class="${type === 'summary'
+        ? `facebook-preview__image`
+        : `facebook-preview__fixed-ratio-content`}" />`
+      : `<div class="facebook-preview__image-fallback"></div>`}
           </div>
 
           <div class="facebook-preview__content">
-            <div class="facebook-preview__hostname">${ getHostName(url)}</div>
             <div class="facebook-preview__title">${ title}</div>
             <div class="facebook-preview__description">${ description}</div>
             
