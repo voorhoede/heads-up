dd50f156129d1b4171463ab18592219b885a67eb
Robin Stut <robin@voorhoede.nl>
2019-11-14 09:57:25 +0100

    fix: writing slack async

--- src/slack-preview/slack-preview.js
+++ src/slack-preview/slack-preview.js
@@ -1,25 +1,27 @@
 
+
+
 createPreview()
 
 function createPreview() {
   const params = (new URL(window.location.href)).searchParams
   const title = params.get('title')
   const description = params.get('description')
-  const image = params.get('image')
+  const imgString = params.get('image')
   const url = params.get('url')
   const type = params.get('card')
   const theme = params.get('theme')
   const favicon = params.get('favicon')
 
   const slackElement = document.querySelector('[data-slack-preview-card]')
-  slackElement.innerHTML = getSlackMarkup({
+  getslackMarkup({
     title,
     description,
-    image,
+    imgString,
     url,
     type,
     favicon
-  })
+  }).then(html => slackElement.innerHTML = html)
 
   /**
    * setting 'theme_dark' class if parent window is in dark mode
@@ -30,28 +32,47 @@ function createPreview() {
 }
 
 
-function getSlackMarkup({ title, description, image, url, type, favicon }) {
-  function getHostName(url) {
-    if (!url) {
-      return ''
-    }
-    const hostname = (new URL(url).hostname)
-    const wwwPrefix = 'www.'
-    return hostname.startsWith(wwwPrefix)
-      ? hostname.slice(wwwPrefix.length)
-      : hostname
+function getslackMarkup({ title, description, imgString, url, type, favicon }) {
+  // const slackLink = url ? `href="${url}"` : ''
+
+  function getMeta(url) {
+    return new Promise((resolve) => {
+      var img = new Image();
+      img.src = url;
+      img.onload = () => resolve(img)
+    })
   }
 
-  function emojiCount() {
-    return Math.floor(Math.random() * 10) + 1
-  }
+  return getMeta(imgString).then(img => generateHtml({ img, title, description, imgString, url, type, favicon }))
+}
 
-  function getImageWeight(img) {
+function getHostName(url) {
+  if (!url) {
     return ''
   }
 
+  const hostname = (new URL(url).hostname)
+  const wwwPrefix = 'www.'
+  return hostname.startsWith(wwwPrefix)
+    ? hostname.slice(wwwPrefix.length)
+    : hostname
+
+}
+
+function emojiCount() {
+  return Math.floor(Math.random() * 10) + 1
+}
+
+function getImageWeight(img) {
+  console.log(img);
+  return ''
+}
+
+function generateHtml({ img, title, description, imgString, url, type, favicon }) {
+  const isSquare = img.width === img.height
+  console.log(`isSquare: ${isSquare}`);
   return `
-      <div class="${ type === 'summary' ? `slack-preview is-small` : `slack-preview`}">
+        <div class="${ type === 'summary' ? `slack-preview is-small` : `slack-preview`}">
       <span class="slack-preview__sidebar"></span>
         <a rel="noopener" target="_blank" class="${type === 'summary' ? `slack-preview__link-container` : `slack-preview__link-container slack-preview__link-container--vertical`} ">
         <div class="slack-preview__content">
@@ -59,14 +80,14 @@ function getSlackMarkup({ title, description, image, url, type, favicon }) {
           <div class="slack-preview__hostname"><img class="slack-preview__favicon" src="${favicon}" alt="">${getHostName(url)}</div></div>
       </div>
           <div class="slack-preview__title">${ title}</div>
-          <div class="slack-preview__description">${ description}${getImageWeight(image)}</div>
-          <div class="${ image
+          <div class="slack-preview__description">${ description}${getImageWeight(imgString)}</div>
+          <div class="${ imgString
       ? `${type === 'summary'
         ? `slack-preview__media`
         : `slack-preview__fixed-ratio slack-preview__ratio`}`
       : `slack-preview__media slack-preview__media--image-fallback`}">
-            ${ image
-      ? `<img src="${image}" class="${type === 'summary'
+            ${ imgString
+      ? `<img src="${imgString}" class="${type === 'summary'
         ? `slack-preview__image`
         : `slack-preview__fixed-ratio-content`}" />`
       : `<div class="slack-preview__image-fallback"></div>`}
