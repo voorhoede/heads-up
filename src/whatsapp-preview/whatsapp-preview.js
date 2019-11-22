e57022e0f8930fc53dcf3ee1d83fcacc55c8646e
Robin Stut <robin@voorhoede.nl>
2019-11-22 13:11:38 +0100

    fix: updating rule show style

--- src/whatsapp-preview/whatsapp-preview.js
+++ src/whatsapp-preview/whatsapp-preview.js
@@ -38,23 +38,25 @@ function getHostName(url) {
 function getwhatsappMarkup({ title, description, image, url, type, imageIsBigEnough }) {
 
   return `
-      <div class="${ type === 'summary' ? `whatsapp-preview is-small` : `whatsapp-preview`}">
-        <a rel="noopener" target="_blank" class="">
+      <div class="whatsapp-preview">
+        <a rel="noopener" target="_blank">
         <div class="whatsapp-top">  
           ${ image !== 'undefined' && imageIsBigEnough === 'true' ? `<div class="whatsapp-preview__media"><img src="${image}" class="whatsapp-preview__image" /></div>` : ``}
+          <div>
           <div class="whatsapp-preview__content">
-            ${title !== 'undefined' ? `<div class="whatsapp-preview__title">${title}</div>` : ''}
-            <div class="whatsapp-preview__description">${description}</div>
-            <div class="whatsapp-preview__url">${ getHostName(url)}</div>
+          ${title !== 'undefined' ? `<div class="whatsapp-preview__title">${title}</div>` : ''}
+          <div class="whatsapp-preview__description">${description}</div>
+          </div>
+          <div class="whatsapp-preview__url">${ getHostName(url)}</div>
           </div>
           </div>
           <div class="whatsapp-preview__domain">
               <div class="whatsapp-preview__hostname">${ url}
-              <div class="whatsapp-preview__timewrapper">
+              <span class="whatsapp-preview__timewrapper">
                 <time class="whatsapp-preview__time">${currentTime()}</time>   
                 <span class="whatsapp-preview__checkmark"><svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 15" width="16" height="15"><path fill="#4FC3F7" d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.879a.32.32 0 0 1-.484.033l-.358-.325a.319.319 0 0 0-.484.032l-.378.483a.418.418 0 0 0 .036.541l1.32 1.266c.143.14.361.125.484-.033l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.879a.32.32 0 0 1-.484.033L1.891 7.769a.366.366 0 0 0-.515.006l-.423.433a.364.364 0 0 0 .006.514l3.258 3.185c.143.14.361.125.484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z"></path></svg>
                 </span> 
-          </div>
+          </span>
               </div>
               
         </a>
