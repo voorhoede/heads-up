d1d74d447631242d4e97396ac4ef3a3b28c6ae48
Robin Stut <robin@voorhoede.nl>
2019-12-02 16:35:24 +0100

    fix: npm run lint

--- src/heads-up/components/switch-buttons.vue
+++ src/heads-up/components/switch-buttons.vue
@@ -6,16 +6,22 @@
       ref="buttons"
       class="switch"
     >
-      <span ref="button" class="switch__text-left">{{ button.label }}</span>
+      <span
+        ref="button"
+        class="switch__text-left"
+      >{{ button.label }}</span>
       <input
         id="input-switch"
         :checked="value === button.value"
         name="toggle"
         type="radio"
         @input="toggle(button.value)"
-      />
+      >
     </label>
-    <span class="control-indicator" :style="controlIndicatorStyle" />
+    <span
+      class="control-indicator"
+      :style="controlIndicatorStyle"
+    />
   </div>
 </template>
 
