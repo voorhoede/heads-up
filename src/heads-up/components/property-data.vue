dec9e16a10ca4b4911355d8ee2f5fb743d9f20ec
Robin Stut <robin@voorhoede.nl>
2019-12-23 14:50:00 +0100

    refactor: value === null to !this.value

--- src/heads-up/components/property-data.vue
+++ src/heads-up/components/property-data.vue
@@ -7,95 +7,109 @@ export default {
   props: {
     exist: {
       type: Boolean,
-      default: true,
+      default: true
     },
     required: {
       type: Boolean,
-      default: false,
+      default: false
     },
     type: {
       type: String,
-      default: null,
+      default: null
     },
     tag: {
       type: String,
-      default: null,
+      default: null
     },
     value: {
       type: String,
-      default: null,
+      default: null
     },
     valueLength: {
       type: Object,
-      default: () => ({ })
+      default: () => ({})
     },
-    hasVariation:{
+    hasVariation: {
       type: Boolean,
-      default: false,
+      default: false
     },
     requiredSizes: {
       type: Object,
-      default: () => ({ })
+      default: () => ({})
     },
     size: {
       type: Object,
-      default: () => ({ })
-    },
+      default: () => ({})
+    }
   },
- computed: {
-    tooltipMessage () {
-      let result = ``
+  computed: {
+    tooltipMessage() {
+      let result = ``;
 
-      this.required ? result += this.isRequired(this.tag) : ''
-      this.exist ? '' : result += this.isDefined(this.type)
+      this.required ? (result += this.isRequired(this.tag)) : "";
+      this.exist ? "" : (result += this.isDefined(this.type));
 
       if (this.valueLength !== undefined) {
-          this.valueLength.tooLong ? result += this.isTooLong(this.valueLength.max) : ''
+        this.valueLength.tooLong
+          ? (result += this.isTooLong(this.valueLength.max))
+          : "";
       }
 
       if (this.type === "og:image") {
-          result += this.value === null ? this.isCorrectUrl(this.type, this.exist) : ''
-          result += this.isBigImg(this.type, this.hasVariation, this.size, this.requiredSizes)
+        result += !this.value ? this.isCorrectUrl(this.type, this.exist) : "";
+        result += this.isBigImg(
+          this.type,
+          this.hasVariation,
+          this.size,
+          this.requiredSizes
+        );
       }
 
       if (result.length === 0) {
-        result += `The ${this.type} tag is perfectly implemented. `
+        result += `The ${this.type} tag is perfectly implemented. `;
       }
 
-      return result
-    },
+      return result;
+    }
   },
   methods: {
     isDefined(tag) {
-      const isImg = tag === 'og:image'
-      const metaTagContent = isImg ? 'Your source' : 'Your content'
+      const isImg = tag === "og:image";
+      const metaTagContent = isImg ? "Your source" : "Your content";
 
-      return `There is no ${tag} defined. You can create the ${tag} in the <head> like <meta property="${tag}" content="${metaTagContent}."> `
+      return `There is no ${tag} defined. You can create the ${tag} in the <head> like <meta property="${tag}" content="${metaTagContent}."> `;
     },
     isTooLong(length) {
-      return `The content has more than ${length} characters. Consider shorten your content. `
+      return `The content has more than ${length} characters. Consider shorten your content. `;
     },
     isRequired(tag) {
-      return `The ${tag} is required to create an unfurling link on this platform. `
+      return `The ${tag} is required to create an unfurling link on this platform. `;
     },
     isBigImg(tag, smallVariant, imgSize, requiredSize) {
-      if (smallVariant && requiredSize.variation.width > imgSize.width && requiredSize.variation.height > imgSize.height) {
-        return`The ${tag} is ${imgSize.width} by ${imgSize.height}px, where ${requiredSize.variation.width} by ${requiredSize.variation.height}px is required for a big unfurling preview. `
+      if (
+        smallVariant &&
+        requiredSize.variation.width > imgSize.width &&
+        requiredSize.variation.height > imgSize.height
+      ) {
+        return `The ${tag} is ${imgSize.width} by ${imgSize.height}px, where ${requiredSize.variation.width} by ${requiredSize.variation.height}px is required for a big unfurling preview. `;
       }
 
-      if (requiredSize.minimum.width > imgSize.width || requiredSize.minimum.height > imgSize.height){
-        return `The ${tag} sizes are too small for a preview. You need at least an image of ${requiredSize.minimum.width} by ${requiredSize.minimum.height}px. `
+      if (
+        requiredSize.minimum.width > imgSize.width ||
+        requiredSize.minimum.height > imgSize.height
+      ) {
+        return `The ${tag} sizes are too small for a preview. You need at least an image of ${requiredSize.minimum.width} by ${requiredSize.minimum.height}px. `;
       }
 
-      return ""
+      return "";
     },
-    isCorrectUrl(tag, correct){
+    isCorrectUrl(tag, correct) {
       if (correct) {
-          return ""
+        return "";
       }
 
-      return `The ${tag} URL can't be reached. `
-    },
+      return `The ${tag} URL can't be reached. `;
+    }
   }
-}
+};
 </script>
