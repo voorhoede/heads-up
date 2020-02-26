7bc801740551d8361e9779721db7945dcee037ec
Robin Stut <robin@voorhoede.nl>
2020-02-26 13:39:06 +0100

    refactor(validate-error-schema.js): removing unused checks

--- src/heads-up/lib/validate-error-schema.js
+++ src/heads-up/lib/validate-error-schema.js
@@ -13,42 +13,10 @@ export default function validateErrorSchema({ schema, key, value }) {
     })
   }
 
+  // Value
   if (!value && !schema[key].required) {
     return null
   }
-
-  // Enum
-  if (schema[key].enum && schema[key].enum.length && !schema[key].enum.includes(valueTrimmed)) {
-    errors.push({
-      message: schema[key].message.enum
-    })
-  }
-
-  // Match
-  if (schema[key]['match'] && !valueTrimmed.match(schema[key]['match'])) {
-    errors.push({
-      message: schema[key].message.match,
-    })
-  }
-
-  // Length
-  if (schema[key]['length'] && valueTrimmed.length > schema[key]['length'].max) {
-    errors.push({
-      message: schema[key].message['length'],
-      length: valueTrimmed.length - schema[key]['length'].max
-    })
-  }
-
-  // Use
-  // if (schema[key].use && schema[key].use.length) {
-  //   schema[key].use.forEach(item => {
-  //     if (!use[item](valueTrimmed, attrs)) {
-  //       errors.push({
-  //         message: schema[key].message.use[item],
-  //       })
-  //     }
-  //   })
-  // }
 
   if (errors.length) {
     return errors
