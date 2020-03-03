ba47d914e704002a8ee5a2fd368f765bfd90d9a1
Robin Stut <robin@voorhoede.nl>
2020-03-03 17:26:21 +0100

    feat(validate-warning-schema.js): valide use.js in warning validation

--- src/heads-up/lib/validate-warning-schema.js
+++ src/heads-up/lib/validate-warning-schema.js
@@ -1,5 +1,7 @@
+import use from './use'
+
 // eslint-disable-next-line sonarjs/cognitive-complexity
-export default function validateWarningSchema({ schema, key, value }) {
+export default function validateWarningSchema({ schema, key, value, attrs }) {
   const valueTrimmed = (value && value.length) ? value.trim() : value
   let warnings = []
 
@@ -9,6 +11,17 @@ export default function validateWarningSchema({ schema, key, value }) {
       valueTrimmed.split(' ').length < schema[key]['descriptive-words']) {
     warnings.push({
       message: schema[key].message['descriptive-words'],
+    })
+  }
+
+    // Use.js check on warnings
+  if (schema[key].warning && schema[key].warning.length) {
+    schema[key].warning.forEach(item => {
+      if (!use[item](valueTrimmed, attrs)) {
+        warnings.push({
+          message: schema[key].message.warning[item],
+        })
+      }
     })
   }
 
