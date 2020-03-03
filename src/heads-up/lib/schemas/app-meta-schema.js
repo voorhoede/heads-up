099d461638a7b082ec6b9c0a9ca365611b903290
Robin Stut <robin@voorhoede.nl>
2020-03-03 17:27:33 +0100

    fix(app-meta-schema.js): split warning from error

--- src/heads-up/lib/schemas/app-meta-schema.js
+++ src/heads-up/lib/schemas/app-meta-schema.js
@@ -33,10 +33,13 @@ const appMetaSchema = {
       required: `The charset attribute is required.`,
       error: {
         hasCorrectFormattedCharset: 'The charset value should be <code>utf-8</code>, not any other values such as <code>utf8</code>.',
+      },
+      warning: {
         isDefinedInCharsetAttribute: 'Always use <code>&lt;meta charset="utf-8"&gt;</code> as It’s backwards compatible and works in all known browsers, so it should always be used over the old <code>&lt;meta http-equiv="Content-Type" content="text/html;charset=UTF-8"&gt;</code>.',
       }
     },
-    error: ['hasCorrectFormattedCharset', 'isDefinedInCharsetAttribute'],
+    error: ['hasCorrectFormattedCharset'],
+    warning: ['isDefinedInCharsetAttribute'],
     meta: {
       info: `The meta attribute <code>charset</code> specifies which character set a web page is written with.`,
       link: 'https://webhint.io/docs/user-guide/hints/hint-meta-charset-utf-8/'
