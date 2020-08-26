c5451f10d7a274f76bd636c623ec749129d1baaf
Vincent van Dijck <vincent@voorhoede.nl>
2020-08-26 12:37:27 +0200

    fix(config) destructure merge require

--- config/create.js
+++ config/create.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const { VueLoaderPlugin } = require('vue-loader')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const path = require('path');
