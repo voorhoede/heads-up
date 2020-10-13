1792e2ead3855c6a275f7e0ceaceca9b333db7d6
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2020-10-13 15:34:27 +0200

    feat(web-app): build the api

--- web-app/functions/api/api.js
+++ web-app/functions/api/api.js
@@ -1,15 +1,52 @@
+const scrape = require('html-metadata')
+
 exports.handler = async function(event) {
-  try {
+  const url = event.queryStringParameters.url
+
+  if (!url) {
     return {
-      statusCode: 200,
-      body: JSON.stringify({ msg: 'Hi.' }, null, 2)
+      statusCode: 400,
+      body: JSON.stringify(
+        { statusCode: 400, error: 'No URL provided.' },
+        null,
+        2
+      )
     }
-  } catch (error) {
-    console.log(error)
+  }
+
+  if (!url.startsWith('http') || !url.startsWith('https')) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        { statusCode: 400, error: "Invalid URL, it should start with 'http' or 'https'." },
+        null,
+        2
+      )
+    }
+  }
+
+  try {
+    const options = {
+      url,
+      headers: { 'User-Agent': 'webscraper' }
+    }
+
+    const metadata = await scrape(options)
 
     return {
-      statusCode: 500,
-      body: JSON.stringify({ msg: error.message }, null, 2)
+      statusCode: 200,
+      body: JSON.stringify({ ...metadata }, null, 2)
+    }
+  } catch (error) {
+    const statusCode = error.status || 500
+
+    return {
+      statusCode,
+      body: JSON.stringify(
+        { statusCode, error: error.message },
+        null,
+        2
+      )
     }
   }
 }
