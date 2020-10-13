3bfc31a8480da6536598d4a412ea8d4748e6821c
Bob Bol <bob@voorhoede.nl>
2020-10-13 13:09:22 +0200

    clean out vue starter stuff

---
+++ web-app/src/components/Shell/Shell.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+  </div>
+  <router-view/>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ web-app/src/components/Shell/ShellSidebar.vue
@@ -0,0 +1,15 @@
+<template>
+<nav class="sidebar">
+
+</nav>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
