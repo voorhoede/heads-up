5d72203d49e84db43b5a16f9aca08b1dfc666b97
Bob Bol <bob@voorhoede.nl>
2020-10-19 11:09:12 +0200

    remove all old @shared

--- old-@shared/lib/message-actions.js
+++
@@ -1,3 +0,0 @@
-export const CONTENT_CHANGED = 'content-change'
-export const GET_DATA = 'get-data'
-export const NEW_DATA = 'new-data'
--- old-@shared/lib/theme.js
+++
@@ -1,7 +0,0 @@
-function getTheme() {
-    const systemTheme = window.matchMedia('(prefers-color-scheme:dark )').matches ? 'dark' : 'light'
-    const devtoolsTheme = chrome.devtools.panels.themeName
-    return (devtoolsTheme === 'systemPreferred') ? systemTheme : devtoolsTheme
-  }
-
-export default getTheme
