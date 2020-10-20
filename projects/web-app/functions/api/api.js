317dc527c1a54c79b8c253651f311baf7601393f
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2020-10-20 17:19:27 +0200

    feat(webapp-api): normalize the webapp api

--- projects/web-app/functions/api/api.js
+++ projects/web-app/functions/api/api.js
@@ -1,11 +1,71 @@
-const scrape = require('html-metadata');
+const { parse } = require('tldts');
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+const robotsParser = require('robots-txt-parser')({ allowOnNeutral: false });
+const xmlJs = require('xml-js');
+
+function elementsToJson(elements) {
+  return Array.from(elements).map(element => {
+    return Object.keys(element.attribs).reduce((obj, attr) => {
+      return { ...obj, [attr]: element.attribs[attr] };
+    }, {});
+  });
+}
+
+async function createHeadResponse(domain, url) {
+  const dom = await fetch(url)
+    .then(res => res.text())
+    .then(dom => {
+      const $ = cheerio.load(dom);
+
+      return {
+        title: $('title').text(),
+        lang: $('html').attr('lang'),
+        link: $('link').get(),
+        meta: $('meta').get(),
+      };
+    });
+
+  return {
+    domain: domain,
+    url: url,
+    title: dom.title,
+    lang: dom.lang,
+    link: elementsToJson(dom.link),
+    meta: elementsToJson(dom.meta),
+  };
+}
+
+function createSitemapResponse(domain) {
+  return fetch(`https://${domain}/sitemap.xml`)
+    .then(res => res.text())
+    .then(text => {
+      const json = xmlJs.xml2json(text);
+
+      return xmlJs.json2xml(json, { spaces: 2 });
+    })
+    .catch(() => null);
+}
+
+function createRobotsResponse(domain) {
+  return robotsParser.fetch(domain).then(result => {
+    if (result.sitemaps) {
+      delete result.sitemaps;
+    }
+
+    return Object.keys(result).map(key => ({
+      name: key,
+      ...result[key],
+    }));
+  });
+}
 
 exports.handler = async function(event) {
   const url = event.queryStringParameters.url;
   let body = null;
 
   if (!url) {
-    body = { error: 'No URL provided.' }
+    body = { error: 'No URL provided.' };
 
     return {
       statusCode: 400,
@@ -23,15 +83,19 @@ exports.handler = async function(event) {
   }
 
   try {
-    const options = {
-      url,
-      headers: { 'User-Agent': 'webscraper' },
-    };
-    const metadata = await scrape(options);
+    const { domain } = parse(url);
+    const urlIsCrawlable = await robotsParser.canCrawl(url);
 
     return {
       statusCode: 200,
-      body: JSON.stringify(metadata),
+      body: JSON.stringify(
+        {
+          urlIsCrawlable,
+          head: await createHeadResponse(domain, url),
+          robots: await createRobotsResponse(domain),
+          sitemap: await createSitemapResponse(domain),
+        }
+      ),
     };
   } catch (error) {
     const statusCode = error.status || 500;
