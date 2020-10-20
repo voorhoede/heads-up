bd051d573a3d59ebf985809c140efd13aa829cf3
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2020-10-20 14:35:07 +0200

    feat(webapp-api): expand the webapp api

--- projects/web-app/functions/api/api.js
+++ projects/web-app/functions/api/api.js
@@ -1,4 +1,8 @@
+const { parse } = require('tldts');
+const fetch = require('node-fetch');
+const robotsParser = require('robots-txt-parser')({ allowOnNeutral: false });
 const scrape = require('html-metadata');
+const xmlJs = require('xml-js');
 
 exports.handler = async function(event) {
   const url = event.queryStringParameters.url;
@@ -23,15 +27,40 @@ exports.handler = async function(event) {
   }
 
   try {
-    const options = {
-      url,
-      headers: { 'User-Agent': 'webscraper' },
-    };
+    const { domain } = parse(url);
+    const options = { url, headers: { 'User-Agent': 'webscraper' } };
     const metadata = await scrape(options);
+
+    const urlIsCrawlable = await robotsParser.canCrawl(url);
+    const robotsTxt = await robotsParser.fetch(domain).then(result => {
+      if (result.sitemaps) {
+        delete result.sitemaps;
+      }
+
+      return Object.keys(result).map(key => ({
+        name: key,
+        ...result[key]
+      }));
+    });
+
+    const sitemap = await fetch(`https://${domain}/sitemap.xml`)
+      .then(res => res.text())
+      .then(text => {
+        const json = xmlJs.xml2json(text);
+        // Convert to JSON first to set a fixed indentation
+        // even if source code was minified.
+        return xmlJs.json2xml(json, { spaces: 2 });
+      })
+      .catch(() => null);
 
     return {
       statusCode: 200,
-      body: JSON.stringify(metadata),
+      body: JSON.stringify({
+        urlIsCrawlable,
+        head: metadata,
+        robots: robotsTxt,
+        sitemap,
+      }),
     };
   } catch (error) {
     const statusCode = error.status || 500;
