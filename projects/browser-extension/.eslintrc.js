46fa4f40b98ab6e096ef2be8f4b659330a5d347b
Bob Bol <bob@voorhoede.nl>
2020-10-20 16:46:51 +0200

    eslint plugins also in sub folders

--- projects/browser-extension/.eslintrc.js
+++ projects/browser-extension/.eslintrc.js
@@ -1,13 +1,9 @@
 module.exports = {
-  // plugins: [ 'sonarjs' ],
-
   extends: [
     'plugin:vue/recommended',
-    // 'plugin:sonarjs/recommended',
   ],
 
   rules: {
-    // 'sonarjs/no-duplicate-string': 0,
     'vue/no-v-html': 'off',
   },
 };
