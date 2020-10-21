e553e7c9385abaccddb8c129d4c6b0cfd33e5c55
Vincent van Dijck <vincent@voorhoede.nl>
2020-10-21 11:27:55 +0200

    feat(link-view) use real head data

--- projects/web-app/src/views/link.view.vue
+++ projects/web-app/src/views/link.view.vue
@@ -1,6 +1,6 @@
 <template>
   <panel-section title="Rel">
-    <collapsible-list :data="linkListData_mocked" />
+    <collapsible-list :data="linkMetadata" />
   </panel-section>
 
   <panel-section title="Resources">
@@ -15,6 +15,7 @@
 </template>
 
 <script>
+import { computed } from 'vue';
 import useHead from '@/composables/use-head';
 import PanelSection from '@shared/components/panel-section';
 import CollapsibleList from '@shared/components/collapsible-list';
@@ -23,7 +24,12 @@ import ExternalLink from '@shared/components/external-link.vue';
 export default {
   setup() {
     const headData = useHead().data;
-    return { headData };
+    const linkMetadata = computed(() => headData.value.head.link);
+
+    return {
+      headData,
+      linkMetadata,
+    };
   },
 
   computed: {
