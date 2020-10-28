38b806e2d839c48373950dbaeb4de05d8e810f1c
Vincent van Dijck <vincent@voorhoede.nl>
2020-10-28 13:18:06 +0100

    fix(open-graph) add warning message

--- projects/browser-extension/vue-app/src/views/open-graph.view.vue
+++ projects/browser-extension/vue-app/src/views/open-graph.view.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <panel-section title="Properties">
-      <properties-list>
+      <div v-if="!ogMeta.length" class="warning-message">
+        <WarningIcon class="icon" />
+        <p>No Open Graph properties detected.</p>
+      </div>
+      <properties-list v-else>
         <properties-item
           v-for="(item, index) in ogMeta"
           :key="index"
@@ -34,9 +38,16 @@ import PanelSection from '@shared/components/panel-section';
 import ExternalLink from '@shared/components/external-link';
 import PropertiesItem from '@shared/components/properties-item';
 import PropertiesList from '@shared/components/properties-list';
+import WarningIcon from '@shared/assets/icons/warning.svg';
 
 export default {
-  components: { ExternalLink, PanelSection, PropertiesItem, PropertiesList },
+  components: {
+    ExternalLink,
+    PanelSection,
+    PropertiesItem,
+    PropertiesList,
+    WarningIcon,
+  },
   computed: {
     ...mapState([ 'head' ]),
     ogMeta() {
