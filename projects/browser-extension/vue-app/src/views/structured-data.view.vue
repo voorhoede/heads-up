0f3a67672f36327a10fb03fc4907aa76dbeadb10
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2021-01-12 11:59:42 +0100

    feat(structured-data): update rendering part 1

--- projects/browser-extension/vue-app/src/views/structured-data.view.vue
+++ projects/browser-extension/vue-app/src/views/structured-data.view.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-    <panel-section title="Properties">
-      <pre>
-{{ structuredData }}
-      </pre>
+    <panel-section v-if="hasMicrodata" title="Microdata">
+      <pre>{{ structuredData.microdata }}</pre>
     </panel-section>
+    <panel-section v-if="hasJsonld" title="JSON-LD">
+      <pre>{{ structuredData.jsonld }}</pre>
+    </panel-section>
+    <panel-section v-if="hasRdfa" title="RDFa">
+      <pre>{{ structuredData.rdfa }}</pre>
+    </panel-section>
+
+    <panel-section v-if="hasNoData" title="Structured Data">
+      <div class="warning-message">
+        <WarningIcon class="icon" />
+        <p>No Structured Data found.</p>
+      </div>
+    </panel-section>
+
     <panel-section title="Resources">
       <ul class="resource-list">
         <li>
           <external-link href="https://schema.org/">
             Schema.org
+          </external-link>
+        </li>
+        <li>
+          <external-link href="https://json-ld.org/">
+            Json-ld.org
+          </external-link>
+        </li>
+        <li>
+          <external-link href="http://rdfa.info/">
+            Rdfa.info
           </external-link>
         </li>
       </ul>
@@ -21,14 +43,37 @@
 import { mapState } from 'vuex';
 import ExternalLink from '@shared/components/external-link';
 import PanelSection from '@shared/components/panel-section';
+import WarningIcon from '@shared/assets/icons/warning.svg';
 
 export default {
   components: {
     ExternalLink,
     PanelSection,
+    WarningIcon,
   },
   computed: {
     ...mapState([ 'structuredData' ]),
+    hasMicrodata() {
+      return (
+        this.structuredData.microdata &&
+        Object.keys(this.structuredData.microdata).length > 0
+      );
+    },
+    hasJsonld() {
+      return (
+        this.structuredData.jsonld &&
+        Object.keys(this.structuredData.jsonld).length > 0
+      );
+    },
+    hasRdfa() {
+      return (
+        this.structuredData.rdfa &&
+        Object.keys(this.structuredData.rdfa).length > 0
+      );
+    },
+    hasNoData() {
+      return !this.hasMicrodata && !this.hasJsonld && !this.hasRdfa;
+    },
   },
 };
 </script>
--- projects/web-app/src/views/structured-data.view.vue
+++ projects/web-app/src/views/structured-data.view.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-    <panel-section title="Properties">
-      <pre>
-{{ structuredData }}
-      </pre>
+    <panel-section v-if="hasMicrodata" title="Microdata">
+      <pre>{{ structuredData.microdata }}</pre>
     </panel-section>
+    <panel-section v-if="hasJsonld" title="JSON-LD">
+      <pre>{{ structuredData.jsonld }}</pre>
+    </panel-section>
+    <panel-section v-if="hasRdfa" title="RDFa">
+      <pre>{{ structuredData.rdfa }}</pre>
+    </panel-section>
+
+    <panel-section v-if="hasNoData" title="Structured Data">
+      <div class="warning-message">
+        <WarningIcon class="icon" />
+        <p>No Structured Data found.</p>
+      </div>
+    </panel-section>
+
     <panel-section title="Resources">
       <ul class="resource-list">
         <li>
           <external-link href="https://schema.org/">
             Schema.org
+          </external-link>
+        </li>
+        <li>
+          <external-link href="https://json-ld.org/">
+            Json-ld.org
+          </external-link>
+        </li>
+        <li>
+          <external-link href="http://rdfa.info/">
+            Rdfa.info
           </external-link>
         </li>
       </ul>
@@ -22,19 +44,29 @@ import { computed } from 'vue';
 import useHead from '@/composables/use-head';
 import ExternalLink from '@shared/components/external-link';
 import PanelSection from '@shared/components/panel-section';
+import WarningIcon from '@shared/assets/icons/warning.svg';
 
 export default {
   setup: () => {
     const headData = useHead().data;
     const structuredData = computed(() => headData.value.structuredData);
+    const hasMicrodata = computed(() => Object.keys(structuredData.value.microdata).length > 0);
+    const hasJsonld = computed(() => Object.keys(structuredData.value.jsonld).length > 0);
+    const hasRdfa = computed(() => Object.keys(structuredData.value.rdfa).length > 0);
+    const hasNoData = computed(() => !hasMicrodata.value && !hasJsonld.value && !hasRdfa.value);
 
     return {
       structuredData,
+      hasMicrodata,
+      hasJsonld,
+      hasRdfa,
+      hasNoData,
     };
   },
   components: {
     ExternalLink,
     PanelSection,
+    WarningIcon,
   },
 };
 </script>
