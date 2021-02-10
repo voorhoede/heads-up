7bb96f6376681bc2b3c7413af40509a0d0ea6023
Bob Bol <bob@voorhoede.nl>
2021-02-10 16:50:44 +0100

    try title in svg without it being removed by svgo

--- projects/web-app/vue.config.js
+++ projects/web-app/vue.config.js
@@ -11,7 +11,16 @@ module.exports = {
       .loader('vue-loader-v16')
       .end()
       .use('vue-svg-loader')
-      .loader('vue-svg-loader');
+      .loader('vue-svg-loader')
+      .options({
+        svgo: {
+          plugins: [
+            {
+              removeTitle: false,
+            },
+          ],
+        },
+      });
 
     // Fix ESLint error when importing from external folder
     // config.module
