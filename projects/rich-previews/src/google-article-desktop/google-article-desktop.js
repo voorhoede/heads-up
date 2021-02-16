4ba16e9f75afc6af6dc75d76e9f3f836d1af00f3
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2021-02-16 13:14:10 +0100

    feat(article-preview): add basic validation

--- projects/rich-previews/src/google-article-desktop/google-article-desktop.js
+++ projects/rich-previews/src/google-article-desktop/google-article-desktop.js
@@ -1,16 +1,36 @@
 const app = Vue.createApp({
   setup() {
     const params = new URL(window.location.href).searchParams;
+    const dateModified = params.get('dateModified');
+    const description = params.get('description');
+    const headline = params.get('headline');
+    const image = params.get('image');
+    const publisherLogo = params.get('publisherLogo');
+    const publisherName = params.get('publisherName');
+    const type = params.get('type');
+
+    const hasRequiredData = () => {
+      return (
+        (dateModified && dateModified !== 'undefined') &&
+        (description && description !== 'undefined') &&
+        (headline && headline !== 'undefined') &&
+        (image && image !== 'undefined') &&
+        (publisherLogo && publisherLogo !== 'undefined') &&
+        (publisherName && publisherName !== 'undefined') &&
+        (type && type !== 'undefined')
+      );
+    };
 
     return {
-      dateModified: params.get('dateModified'),
-      description: params.get('description'),
-      headline: params.get('headline'),
-      image: params.get('image'),
+      dateModified,
+      description,
+      hasRequiredData: hasRequiredData(),
+      headline,
+      image,
       isDarkMode: params.get('theme') === 'dark',
-      publisherLogo: params.get('publisherLogo'),
-      publisherName: params.get('publisherName'),
-      type: params.get('type'),
+      publisherLogo,
+      publisherName,
+      type,
     };
   },
 });
