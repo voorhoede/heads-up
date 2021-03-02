7ed3957abb6c6b7b439daf3f821d83b72da1adc0
Vincent van Dijck <vincent@voorhoede.nl>
2021-03-02 15:10:44 +0100

    feat(whatsapp) use new validation setup

--- projects/@shared/views/whatsapp/schema.js
+++ projects/@shared/views/whatsapp/schema.js
@@ -1,58 +1,57 @@
-const whatsappSchema = {
+import Joi from '../../lib/validator';
+
+export const schema = Joi.object({
+  'og:title': Joi.string()
+    .max(65)
+    .required()
+    .messages({
+      'string.max': 'Avoid 65 characters or more.',
+      'string.empty': 'This property is required for rich links on WhatsApp.',
+    }),
+
+  'og:image': Joi.image()
+    .minDimensions(200, 300)
+    .required()
+    .messages({
+      'any.required': 'This property is required for rich links on WhatsApp.',
+    }),
+
+  'og:description': Joi.string()
+    .max(155)
+    .required()
+    .messages({
+      'string.max': 'Avoid 155 characters or more.',
+      'any.required': 'This property is required for rich links on WhatsApp.',
+    }),
+
+  'og:url': Joi.string()
+    .required()
+    .messages({
+      'string.empty': 'This property is required for rich links on WhatsApp.',
+    }),
+
+  'og:type': Joi.string()
+    .allow(''),
+});
+
+export const info = {
   'og:title': {
-    required: true,
-    message: {
-      required: 'This property is required for rich links on WhatsApp.',
-      'max-characters': 'Avoid 65 characters or more.',
-    },
-    meta: {
-      info: 'The <code>og:title</code> element defines the title of your rich link.',
-    },
-    'max-characters': 65,
+    info: 'The <code>og:title</code> element defines the title of your rich link.',
   },
 
   'og:image': {
-    required: true,
-    message: {
-      required: 'This property is required for rich links on WhatsApp.',
-      'image-min-size': 'Avoid images smaller than 300x200 or less.',
-    },
-    'image-min-size': {
-      height: 200,
-      width: 300,
-    },
-    meta: {
-      info: 'The <code>og:image</code> element defines the image of your rich link.',
-    },
+    info: 'The <code>og:image</code> element defines the image of your rich link.',
   },
 
   'og:description': {
-    required: true,
-    message: {
-      required: 'This property is required for rich links on WhatsApp.',
-      'max-characters': 'Avoid 155 characters or more.',
-    },
-    meta: {
-      info: 'The <code>og:description</code> element defines the description of your rich link.',
-    },
-    'max-characters': 155,
+    info: 'The <code>og:description</code> element defines the description of your rich link.',
   },
 
   'og:url': {
-    required: true,
-    message: {
-      required: 'This property is required for rich links on WhatsApp.',
-    },
-    meta: {
-      info: 'The <code>og:url</code> element defines the canonical URL of your rich link.',
-    },
+    info: 'The <code>og:url</code> element defines the canonical URL of your rich link.',
   },
 
   'og:type': {
-    meta: {
-      info: 'The <code>og:type</code> element defines the type of content, whether it’s an article, video, or rich media.',
-    },
+    info: 'The <code>og:type</code> element defines the type of content, whether it’s an article, video, or rich media.',
   },
 };
-
-export default whatsappSchema;
