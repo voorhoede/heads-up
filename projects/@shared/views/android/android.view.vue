730dd0e0161a283d91cc51be7b53a86972fff495
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2021-04-23 16:00:13 +0200

    chore(android): clean up the view file

--- projects/@shared/views/android/android.view.vue
+++ projects/@shared/views/android/android.view.vue
@@ -12,36 +12,13 @@
         </properties-item>
       </properties-list>
     </panel-section>
-    <panel-section title="Touch icons">
-      <div v-if="!touchIcons.length" class="warning-message">
-        <WarningIcon class="icon" />
-        <p>No touch icons detected.</p>
-      </div>
-      <properties-list v-else>
+    <panel-section title="App Links">
+      <properties-list>
         <properties-item
-          v-for="icon in touchIcons"
-          :key="icon.term"
-          :term="icon.term"
-          :value="icon.url"
-          :image="icon"
-          type="image"
-        >
-        </properties-item>
-      </properties-list>
-    </panel-section>
-    <panel-section title="Startup images">
-      <div v-if="!startupImages.length" class="warning-message">
-        <WarningIcon class="icon" />
-        <p>No startup images detected.</p>
-      </div>
-      <properties-list v-else>
-        <properties-item
-          v-for="image in startupImages"
-          :key="image.term"
-          :term="image.term"
-          :value="image.url"
-          :image="image"
-          type="image"
+          v-for="link in appLinks"
+          :key="link.term"
+          :term="link.term"
+          :value="link.url"
         >
         </properties-item>
       </properties-list>
@@ -49,13 +26,13 @@
     <panel-section title="Resources">
       <ul class="resource-list">
         <li>
-          <external-link href="https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariHTMLRef/Articles/MetaTags.html#//apple_ref/doc/uid/TP40008193-SW3">
-            Apple-specific meta tags
+          <external-link href="https://developers.facebook.com/docs/applinks">
+            App Links - Facebook Docs
           </external-link>
         </li>
         <li>
-          <external-link href="https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html">
-            Configuring web applications for iOS
+          <external-link href="https://github.com/joshbuchea/HEAD">
+            HEAD by joshbuchea
           </external-link>
         </li>
       </ul>
@@ -65,14 +42,12 @@
 
 <script>
 import { computed } from 'vue';
-import createAbsoluteUrl from '@shared/lib/create-absolute-url';
 import { findMetaContent } from '@shared/lib/find-meta';
 
 import ExternalLink from '@shared/components/external-link';
 import PanelSection from '@shared/components/panel-section';
 import PropertiesItem from '@shared/components/properties-item';
 import PropertiesList from '@shared/components/properties-list';
-import WarningIcon from '@shared/assets/icons/warning.svg';
 
 export default {
   props: {
@@ -109,37 +84,8 @@ export default {
       ];
     });
 
-    const touchIcons = computed(() => (
-      props.headData.head.link
-        .filter(link => link.rel === 'apple-touch-icon')
-        .map(icon => ({
-          ...icon,
-          url: absoluteUrl(icon.href),
-          term: [ icon.rel, icon.sizes ],
-        }))
-        .sort((a, b) => {
-          const sizeA = a.sizes ? a.sizes.split('x')[0] : 0;
-          const sizeB = b.sizes ? b.sizes.split('x')[0] : 0;
-          return parseInt(sizeA, 10) > parseInt(sizeB, 10) ? 1 : -1;
-        })
-    ));
-
-    const startupImages = computed(() => (
-      props.headData.head.link
-        .filter(link => link.rel === 'apple-touch-startup-image')
-        .map(image => ({
-          ...image,
-          url: absoluteUrl(image.href),
-          term: [ image.rel, image.sizes, image.media ],
-        }))
-    ));
-
-    const absoluteUrl = url => createAbsoluteUrl(props.headData.head, url);
-
     return {
       metaData,
-      touchIcons,
-      startupImages,
     };
   },
   components: {
@@ -147,7 +93,6 @@ export default {
     PanelSection,
     PropertiesItem,
     PropertiesList,
-    WarningIcon,
   },
 };
 </script>
