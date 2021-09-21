9429b4f70d55ccc7f01aa5841c38b07ccafe6dba
Vincent van Dijck <vincent@voorhoede.nl>
2021-09-21 10:50:17 +0200

    chore(deps) update deps to fix failing build

--- projects/web-app/vue.config.js
+++ projects/web-app/vue.config.js
@@ -3,32 +3,6 @@ const resolve = dir => path.join(__dirname, dir);
 
 module.exports = {
   chainWebpack: config => {
-    // Use vue-svg-loaded
-    const svgRule = config.module.rule('svg');
-    svgRule.uses.clear();
-    svgRule
-      .use('vue-loader')
-      .loader('vue-loader-v16')
-      .end()
-      .use('vue-svg-loader')
-      .loader('vue-svg-loader')
-      .options({
-        svgo: {
-          plugins: [
-            { removeTitle: false },
-          ],
-        },
-      });
-
-    // Fix ESLint error when importing from external folder
-    // config.module
-    //   .rule('eslint')
-    //   .use('eslint-loader')
-    //   .tap(options => {
-    //     options.configFile = path.resolve(__dirname, '../../.eslintrc.js');
-    //     return options;
-    //   });
-
     // Add alias for external folder
     config.resolve.alias
       .set('@shared', resolve('../@shared'));
