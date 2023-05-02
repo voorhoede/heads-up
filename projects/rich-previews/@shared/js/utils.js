15a10a02093fbda98091b7a2c4aa2308bac2a7f8
Vincent van Dijck <vincent@voorhoede.nl>
2023-05-02 14:03:40 +0200

    chore(projects): remove rich-previews

--- projects/rich-previews/@shared/js/utils.js
+++
@@ -1,8 +0,0 @@
-export const getHostname = url => {
-  if(!url) return '';
-  const hostname = (new URL(url).hostname);
-  const wwwPrefix = 'www.';
-  return hostname.startsWith(wwwPrefix)
-    ? hostname.slice(wwwPrefix.length)
-    : hostname;
-};
